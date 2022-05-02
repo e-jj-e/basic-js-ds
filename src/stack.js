f69c2528ec7cabceeb2ddbc0880c3393bc8022ec
e-jj-e <101937429+e-jj-e@users.noreply.github.com>
2022-05-02 23:49:08 +0600

    feat: add solution

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor(){
+    this.array=[];
+  }
+  
+  push(element) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.array.push(element);
+    
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.array.pop();
+   
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.array[this.array.length-1];
+   
   }
 }
 
